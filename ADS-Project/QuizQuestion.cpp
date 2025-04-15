f81150e044e9eb9ec68ad42508a564d9c70889f6
SantySantos <santysantos533@gmail.com>
2025-04-15 16:27:57 -0400

    trying to work in question manager

--- ADS-Project/QuizQuestion.cpp
+++ ADS-Project/QuizQuestion.cpp
@@ -13,6 +13,12 @@ std::string QuizQuestion::getQuestion() {
 	return question;
 }
 
+std::string QuizQuestion::setQuestion(const string& tempQuestion) 
+{
+	question = tempQuestion;
+	return question;
+}
+
 bool QuizQuestion::Evaluate() {
 	// This is a placeholder implementation. The actual implementation should be in derived classes.
 	cout << "Evaluating question: " << question << endl;
